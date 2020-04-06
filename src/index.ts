06a4b1db23746756ad476813f86d3678ce9a4d8a
lee <lee@underneath.ca>
2020-04-06 12:54:24 -0700

    export symLinkForce

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-import { compileIfNecessary, jsOutFile, expectFilesExist } from "./compileUtil";
+import {
+  compileIfNecessary,
+  jsOutFile,
+  expectFilesExist,
+  symLinkForce
+} from "./compileUtil";
 import { defaultOutDir, loadTsConfig } from "./loadTsConfig";
 
-export { defaultOutDir, compileIfNecessary, jsOutFile, expectFilesExist, loadTsConfig };
+export {
+  defaultOutDir,
+  compileIfNecessary,
+  jsOutFile,
+  expectFilesExist,
+  loadTsConfig,
+  symLinkForce
+};
